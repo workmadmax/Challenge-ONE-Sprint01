073e455b3dbcbd470be7c564195df3c621f3ea82
workmadmax <work.madmax@gmail.com>
2023-04-16 17:03:01 -0300

    refactor encode decode

--- srcs/js/script.js
+++ srcs/js/script.js
@@ -6,7 +6,7 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 14:48:10 by madmax42          #+#    #+#             */
-/*   Updated: 2023/04/16 16:18:16 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/04/16 17:02:13 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,85 +15,38 @@ const textInput = document.getElementById('text-input');
 const outputText = document.getElementById('output-text-after');
 // Add click events to the buttons
 const btnEncrypt = document.getElementById('btnEncrypt');
+const btnDecrypt = document.getElementById('btnDecrypt');
+
+btnEncrypt.addEventListener('click', () => {
+	const encryptedText = encrypt(textInput.value);
+	outputText.textContent = `After the encoder: ${encryptedText}`;
+});
+
+btnDecrypt.addEventListener('click', () => {
+	const decryptedText = decrypt(textInput.value);
+	outputText.textContent = `After the decoder: ${decryptedText}`;
+});
+
+// Encryption and decryption functions
 
 function encrypt(text)
 {
-	let result = '';
-
-	for (let i = 0; i < text.length; i++)
-	{
-		switch (text[i])
-		{
-			case 'e':
-				result += 'enter';
-				break;
-			case 'i':
-				result += 'imes';
-				break;
-			case 'a':
-				result += 'ai';
-				break;
-			case 'o':
-				result += 'ober';
-				break;
-			case 'u':
-				result += 'ufat';
-				break;
-			default:
-				result += text[i];
-		}
-	}
-	return result;
+	const encryptedText = text
+		.replace(/e/g, 'enter')
+		.replace(/i/g, 'imes')
+		.replace(/a/g, 'ai')
+		.replace(/o/g, 'ober')
+		.replace(/u/g, 'ufat');
+	return encryptedText;
 }
 
 function decrypt(text)
 {
-	let result = '';
-	let i = 0;
-	while (i < text.length)
-	{
-		switch (text.slice(i, i + 5))
-		{
-			case 'enter':
-				result += 'e';
-				i += 5;
-				break;
-			case 'imes':
-				result += 'i';
-				i += 4;
-				break;
-			case 'ai':
-				result += 'a';
-				i += 2;
-				break;
-			case 'ober':
-				result += 'o';
-				i += 4;
-				break;
-			case 'ufat':
-				result += 'u';
-				i += 4;
-				break;
-			default:
-				result += text[i];
-				i++;
-		}
-	}
-	return result;
+	const decryptedText = text
+		.replace(/enter/g, 'e')
+		.replace(/imes/g, 'i')
+		.replace(/ai/g, 'a')
+		.replace(/ober/g, 'o')
+		.replace(/ufat/g, 'u');
+	return decryptedText;
 }
-
-
-
-btnEncrypt.addEventListener('click', () =>
-{
-  const encryptedText = encrypt(textInput.value);
-  outputText.textContent = `After the encoder: ${encryptedText}`;
-});
-
-const btnDecrypt = document.getElementById('btnDecrypt');
-
-btnDecrypt.addEventListener('click', () =>
-{
-  const decryptedText = decrypt(textInput.value);
-  outputText.textContent = `After the decoder: ${decryptedText}`;
-});
