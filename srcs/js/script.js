206c3b32f9ff8e3071a8131f2247967d908ef166
workmadmax <work.madmax@gmail.com>
2023-04-17 10:33:34 -0300

    refactoring code

--- srcs/js/script.js
+++ srcs/js/script.js
@@ -6,99 +6,87 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 14:48:10 by madmax42          #+#    #+#             */
-/*   Updated: 2023/04/16 20:35:47 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/04/17 10:32:06 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Get the elements from the DOM
 const textInput = document.getElementById('text-input');
 const outputText = document.getElementById('output-text-after');
-// Add click events to the buttons
 const btnEncrypt = document.getElementById('btnEncrypt');
 const btnDecrypt = document.getElementById('btnDecrypt');
 
 function validateInput(str)
 {
-    const normalizedStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    return /^[a-z\s]+$/.test(normalizedStr);
+	const normalizedStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	return /^[a-z\s]+$/.test(normalizedStr);
+}
+
+function addCopyButton(text)
+{
+	let copyButton = document.getElementById('copy-button');
+
+	if (!copyButton)
+	{
+		copyButton = document.createElement('button');
+		copyButton.id = 'copy-button';
+		copyButton.textContent = 'Copy';
+		copyButton.style.backgroundColor = 'blue';
+		copyButton.style.color = 'white';
+		copyButton.style.borderRadius = '5px';
+		copyButton.style.padding = '10px';
+		copyButton.style.marginLeft = '10px';
+		copyButton.style.marginTop = '10px';
+		outputText.insertAdjacentElement('afterend', copyButton);
+	}
+	copyButton.removeEventListener('click', copyEventHandler);
+	copyButton.addEventListener('click', copyEventHandler);
+	function copyEventHandler()
+	{
+		navigator.clipboard.writeText(text);
+	}
 }
 
 btnEncrypt.addEventListener('click', () =>
 {
-    if(/[A-Z]/.test(textInput.value))
+	if (/[A-Z]/.test(textInput.value))
 	{
-        alert('The input should not contain uppercase letters');
-        return;
-    }
-    const textToEncrypt =
+		alert('The input should not contain uppercase letters');
+		return;
+	}
+	const textToEncrypt =
 			textInput.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-    if (!validateInput(textToEncrypt))
+	if (!validateInput(textToEncrypt))
 	{
-        alert('Only lowercase letters and no accent!');
-        return;
-    }
+		alert('Only lowercase letters and no accent!');
+		return;
+	}
 	const encryptedText = encrypt(textToEncrypt);
 	outputText.textContent = `${encryptedText}`;
 	const alertTitle = document.getElementById('alert-title');
 	const alertMsg = document.getElementById('alert-msg');
 	alertTitle.style.display = 'none';
 	alertMsg.style.display = 'none';
-
-	// Check if copy button already exists
-	let copyButton = document.getElementById('copy-button');
-	if (!copyButton) {
-		copyButton = document.createElement('button');
-		copyButton.id = 'copy-button';
-		copyButton.textContent = 'Copy';
-		copyButton.style.backgroundColor = 'blue';
-		copyButton.style.color = 'white';
-		copyButton.style.borderRadius = '5px';
-		copyButton.style.padding = '10px';
-		copyButton.style.marginLeft = '10px';
-		copyButton.style.marginTop = '10px';
-		copyButton.addEventListener('click', () => {
-			navigator.clipboard.writeText(encryptedText);
-			alert('Encrypted text copied to clipboard!');
-		});
-		outputText.insertAdjacentElement('afterend', copyButton);
-	}
+	addCopyButton(encryptedText);
+	textInput.value = '';
 });
 
 btnDecrypt.addEventListener('click', () =>
 {
-    if(/[A-Z]/.test(textInput.value))
+	if (/[A-Z]/.test(textInput.value))
 	{
-        alert('The input should not contain uppercase letters');
-        return;
-    }
+		alert('The input should not contain uppercase letters');
+		return;
+	}
 	const textToDecrypt =
 			textInput.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
 	const decryptedText = decrypt(textToDecrypt);
 	outputText.textContent = `${decryptedText}`;
-
 	const alertTitle = document.getElementById('alert-title');
 	const alertMsg = document.getElementById('alert-msg');
 	alertTitle.style.display = 'none';
 	alertMsg.style.display = 'none';
-
-	// Check if copy button already exists
-	let copyButton = document.getElementById('copy-button');
-	if (!copyButton) {
-		copyButton = document.createElement('button');
-		copyButton.id = 'copy-button';
-		copyButton.textContent = 'Copy';
-		copyButton.style.backgroundColor = 'blue';
-		copyButton.style.color = 'white';
-		copyButton.style.borderRadius = '5px';
-		copyButton.style.padding = '10px';
-		copyButton.style.marginLeft = '10px';
-		copyButton.style.marginTop = '10px';
-		copyButton.addEventListener('click', () => {
-			navigator.clipboard.writeText(decryptedText);
-			alert('Decrypted text copied to clipboard!');
-		});
-		outputText.insertAdjacentElement('afterend', copyButton);
-	}
+	addCopyButton(decryptedText);
+	textInput.value = '';
 });
 
 // Encryption and decryption functions
